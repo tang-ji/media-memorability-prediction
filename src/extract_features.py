ca069f3abe7d33887df68d3c5b81297f825b28e5
tangji08 <ji.tang@polytechnique.edu>
2020-08-02 23:34:39 +0200

    Update extract_features.py

--- src/extract_features.py
+++ src/extract_features.py
@@ -9,36 +9,31 @@ import numpy as np
 
 IMG_SIZE=(224, 224)
 
-def get_features_from_image_list(images_list, img_path, h5_filename, batch_size=16, symmetric=False):
+def get_features_from_image_list(images_list, img_path, batch_size=16, symmetric=False):
     model = ResNet152(weights="imagenet", include_top=False, pooling='avg')
     size = 2048
     widgets = ["Extracting Features: ", progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
     pbar = progressbar.ProgressBar(maxval=len(images_list), widgets=widgets).start()
-    with h5py.File(h5_filename, 'w') as h5f:
-        for i in np.arange(0, len(images_list), batch_size):
-            batch_paths = images_list[i:i + batch_size]
-            batch_images = []
-            for img in batch_paths:
-                image = load_img(img_path + img, target_size=IMG_SIZE)
-                image = img_to_array(image)
-                if symmetric:
-                    image = np.array(list(map(lambda x:x[::-1], image)))
-                image = np.expand_dims(image, axis=0)
-                batch_images.append(image)
-                
-            batch_images = np.vstack(batch_images)
-            features = model.predict(batch_images, batch_size=batch_size)
-            features = features.reshape((features.shape[0], size))
-            try:
-                pbar.update(i)
-            except:
-                pass
-            for j, img in enumerate(batch_paths):
-                h5f.create_dataset(img, data=features[j])
+    features_full = []
+    for i in np.arange(0, len(images_list), batch_size):
+        batch_paths = images_list[i:i + batch_size]
+        batch_images = []
+        for img in batch_paths:
+            image = load_img(img_path + img, target_size=IMG_SIZE)
+            image = img_to_array(image)
+            if symmetric:
+                image = np.array(list(map(lambda x:x[::-1], image)))
+            image = np.expand_dims(image, axis=0)
+            batch_images.append(image)     
+
+        batch_images = np.vstack(batch_images)
+        features = model.predict(batch_images, batch_size=batch_size)
+        features = features.reshape((features.shape[0], size))
+        features_full.append(features)
+        try:
+            pbar.update(i)
+        except:
+            pass
     pbar.finish()
 
-    with h5py.File(h5_filename) as h5f:
-
-        features = np.array([h5f[img][:] for img in images_list])
-
-    return features
+    return np.vstack(features_full)
