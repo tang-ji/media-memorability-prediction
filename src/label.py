e890378c9646f5c145eb4698d9400d903ee6b38e
tangji08 <ji.tang@polytechnique.edu>
2020-08-05 12:21:51 +0200

    add label transformation class

---
+++ src/label.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Label_transformer:
+    def __init__(self, Y, r=1.4):
+        self.mean = np.mean(Y)
+        self.std = np.std(Y)
+        self.r = r
+
+    def transform(self, Y):
+        return np.clip((Y-self.mean)/self.std/self.r, -1.0, 1.0)
+
+    def save():
+        np.save("model/label", np.array([self.mean, self.std]))
+
+    def load():
+        self.mean, self.std = np.load("model/label")
